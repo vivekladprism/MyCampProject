0c3034fc933e2bfeb28ec833e612d005aa657c30
vivekladprism <viveklad27@gmail.com>
2021-01-09 00:05:08 -0500

    added images, price and description features with a bit of styling and navs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 const CampGround = require('./models/campgrounds');
 
 mongoose.connect('mongodb://localhost:27017/myCamp', {
@@ -16,8 +18,11 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 
 app.get("/", (req, res) => {
@@ -28,6 +33,41 @@ app.get("/makeCampGround", async (req, res) => {
     const camp = new CampGround({ title: 'My yard' });
     await camp.save();
     res.send(camp);
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await CampGround.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds', async (req, res) => {
+    const campground = new CampGround(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    const campground = await CampGround.findByIdAndUpdate(id, { title: req.body.campground.title, location: req.body.campground.location, price: req.body.campground.price, description: req.body.campground.description, image: req.body.campground.image });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const id = req.params.id;
+    await CampGround.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 })
 app.listen(3000, () => {
     console.log("serving on port 3000");
---
+++ seeds/index.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campgrounds');
+
+mongoose.connect('mongodb://localhost:27017/myCamp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 50) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/featured/?campgrounds',
+            description: "asfanksgnkaf afnskdjngsdk ngsd snfk sdfnksd",
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
